dbaad1a6eca667600d9cbf52d40eaec8521c4d12
Christian Marquez <christianmarquez41@gmail.com>
2021-09-21 18:44:16 -0400

    added statistic place holder values to homepage and created redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '8f649d1839mshb254b3b817c55a0p10f531jsn27bfab8fa440'
+//     }
+//   };
