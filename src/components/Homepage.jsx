e931947faed3efa9509651bb2200b4266a0bd554
Christian Marquez <christianmarquez41@gmail.com>
2021-09-22 11:54:15 -0400

    added info from api to the page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,8 +8,9 @@ const { Title } = Typography;
 
 export default function Homepage() {
   const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats
 
-  console.log(data);
+  if(isFetching) return "loading..."
 
   return (
     <>
@@ -18,21 +19,23 @@ export default function Homepage() {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
     </>
   );
 }
+
+// millify(globalStats.exchanges)
